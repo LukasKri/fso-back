61bb39034e3e1f573c054ddc372ed1222512d7b8
Lukas Krisikaitis <Lukas.Krisikaitis@ibm.com>
2022-05-26 21:15:46 +0300

    Ready to be deployed?

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -104,7 +107,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
